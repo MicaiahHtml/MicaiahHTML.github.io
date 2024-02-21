e1b54f873135a55bad22a6a61bb1ee2035577135
Micaiah <mvins31@wgu.edu>
2024-02-21 17:15:07 -0500

    test commit on new computer

---
+++ src/components/PortfolioItem.jsx
@@ -0,0 +1,9 @@
+import './PortfolioItemStylesheet.css';
+//props.title props.link props.img
+export default function PortfolioItem(props){
+    return(
+        <div id = "portfolio-item-container">
+            <img src = "https://placehold.co/1000x500"/>
+        </div>
+    )
+}
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,13 +1,14 @@
 import SectionHeader from "../components/SectionHeader";
+import PortfolioItem from "../components/PortfolioItem";
 
 export default function Portfolio() {
     return (
       <div id = "portfolio" className = "portfolio-container">
         <SectionHeader>Portfolio</SectionHeader>
-        <img src = "https://placehold.co/1000x500"/>
-        <img src = "https://placehold.co/1000x500"/>
-        <img src = "https://placehold.co/1000x500"/>
-        <img src = "https://placehold.co/1000x500"/>
+        <PortfolioItem/>
+        <PortfolioItem/> 
+        <PortfolioItem/>
+        <PortfolioItem/>
       </div>
     )
   }
